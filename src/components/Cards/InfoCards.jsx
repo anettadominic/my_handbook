68cc6ed99186364ea8cb07728980510863197342
Your Name <your_email@example.com>
2024-12-26 21:48:58 +0530

    img

--- src/components/Cards/InfoCards.jsx
+++ src/components/Cards/InfoCards.jsx
@@ -5,6 +5,7 @@ const InfoCard = ({ image, title, description, buttonText, onButtonClick }) => {
           {image ? (
             <img
               className="w-full h-full object-cover"
+              src={image}
               alt="Card Image"
             />
           ) : (
