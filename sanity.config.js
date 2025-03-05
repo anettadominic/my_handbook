e18c64b425daa4932a4e403f604b3c7ebc0cc6dd
Your Name <your_email@example.com>
2025-03-05 12:43:40 +0530

    updated sanity config file

--- sanity.config.js
+++ sanity.config.js
@@ -3,6 +3,8 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {codeInput} from '@sanity/code-input'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
+
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +13,7 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: process.env.SANITY_STUDIO_DATASET,
 
-  plugins: [structureTool(), visionTool(),codeInput()],
+  plugins: [structureTool(), visionTool(),codeInput(),vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
